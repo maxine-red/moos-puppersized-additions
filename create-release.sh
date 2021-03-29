a1324da6743dfc2c2a15885e16bbe9b6909efe18
Maxine Michalski <maxine_red1@yahoo.com>
2021-03-29 23:12:18 +0200

    Updated upload info

--- create-release.sh
+++ create-release.sh
@@ -3,25 +3,14 @@
 getVersion() {
   readarray -d = -t mc_release<<< "$(grep minecraft_release gradle.properties)"
   readarray -d = -t major<<< "$(grep version_major gradle.properties)"
-  readarray -d = -t api_major<<< "$(grep version_api_major gradle.properties)"
-  readarray -d = -t api_minor<<< "$(grep version_api_minor gradle.properties)"
+  readarray -d = -t minor<<< "$(grep version_minor gradle.properties)"
   readarray -d = -t patch<<< "$(grep version_patch gradle.properties)"
-  full_version=$(echo ${mc_release[1]})-$(echo ${major[1]}).$(echo ${api_major[1]}).$(echo ${api_minor[1]}).$(echo ${patch[1]})
-}
-
-getDescription() {
-  description=$(cat changelog.md)
-}
-
-getRepo() {
-  readarray -d = -t git_repo<<< "$(grep github_repo gradle.properties)"
-  repo=$(echo ${git_repo[1]})
+  full_version="v$(echo ${major[1]}).$(echo ${minor[1]}).$(echo ${patch[1]})"
 }
 
 getVersion
-getDescription
-getRepo
-github-release release --user NovaMachina-Mods --repo "$repo" --tag "$full_version" --name "$full_version" --description "$description"
-echo "Created Release $full_version"
-github-release upload --user NovaMachina-Mods --repo "$repo" --tag "$full_version" --name "$full_version.jar" --file build/libs/*.jar
-echo "Uploaded $full_version.jar to release $full_version"
+echo git checkout main && git merge development && git push && \
+  git tag -a "$full_version"  -F CHANGELOG.md && \
+  echo "Created tag $full_version" && \
+  git push origin "$full_version" && git checkout development && \
+  echo "Pushed new tag $full_version"
