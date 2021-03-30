5a68ad131c7e4c607905d4178ae85c442213295e
Maxine Michalski <maxine_red1@yahoo.com>
2021-03-31 01:17:16 +0200

    Moved upload in release creation

--- create-release.sh
+++ create-release.sh
@@ -17,8 +17,7 @@ git checkout development && git pull && \
   echo "Built release jar" && \
   git push origin "$full_version" && git checkout development && \
   echo "Pushed new tag $full_version" && \
-  gh release create "$full_version" -F CHANGELOG.md && \
+  cd build/libs && \
+  gh release create "$full_version" -F CHANGELOG.md \
+  StrawberryTwirlCompanion-"$(echo $full_version|sed 's/v//')".jar && \
   echo "Created release for $full_version"
-  gh release upload "$full_version" \
-  build/libs/StrawberryTwirlCompanion-"$(echo $full_version|sed 's/v//')".jar && \
-  echo "Uploaded mod file for $full_version"
