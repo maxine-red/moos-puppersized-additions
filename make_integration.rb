5e387cb721de763aec1066b7f999917668dcc563
Maxine Michalski <maxine_red1@yahoo.com>
2021-04-08 12:52:10 +0200

    Overhaul of integration creation

--- make_integration.rb
+++ make_integration.rb
@@ -2,29 +2,32 @@
 
 require 'json'
 
+mod = 'strawberrytwirl'
+
 data = JSON.parse(File.read(File.dirname(__FILE__) + '/integration.json'))
-Dir.chdir(File.dirname(__FILE__)) do
-  Dir.chdir('src/main/resources/data/strawberrytwirl/recipes') do 
-    Dir.mkdir('integration') unless File.exists?('integration')
-    Dir.chdir('integration') do
-      data.each do |k, v|
-        Dir.mkdir(k) unless File.exists?(k)
-        Dir.chdir(k) do
-          recipe = {conditions: [{type: 'forge:mod_loaded', modid: 'create'}]}
-          v.each do |m, d|
-            Dir.mkdir(m) unless File.exists?(m)
-            Dir.chdir(m) do
-              recipe.store(:type, "#{k}:#{m}")
-              d.each do |r|
-                rec = recipe.dup
-                r.each do |key, value|
-                  next if key == 'name'
-                  rec.store(key, value)
-                end
-                File.write(r['name'], JSON.pretty_generate(rec))
-              end
-            end
+dir = "#{File.dirname(__FILE__)}/src/main/resources/data/#{mod}/recipes/integration"
+dir.split('/').each do |d|
+  Dir.mkdir(d) unless File.directory?(d)
+  Dir.chdir(d)
+end
+data.each do |k, v|
+  Dir.mkdir(k) unless File.exists?(k)
+  Dir.chdir(k) do
+    recipe = {conditions: [{type: 'forge:mod_loaded', modid: 'create'}]}
+    v.each do |m, d|
+      Dir.mkdir(m) unless File.exists?(m)
+      Dir.chdir(m) do
+        recipe.store(:type, "#{k}:#{m}")
+        d.each do |i|
+          rec = recipe.dup
+          if m == 'filling'
+            rec.store(:ngredientss, [{fluid: "#{mod}:#{i}", amount: 250}, {item: 'minecraft:glass_bottle'}])
+            rec.store(:results, [{item: "#{mod}:#{i}_bottle"}])
+          elsif m == 'emptying'
+            rec.store(:ingredients, [{item: "#{mod}:#{i}_bottle"}])
+            rec.store(:results, [{fluid: "#{mod}:#{i}", amount: 250}, {item: 'minecraft:glass_bottle'}])
           end
+          File.write("#{i}.json", JSON.pretty_generate(rec))
         end
       end
     end
